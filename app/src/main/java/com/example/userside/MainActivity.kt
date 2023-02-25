831e5aa287a89b4a59c837e6778176d2d6f8069f
Abir Bourbia <ja_bourbia@esi.dz>
2023-02-25 23:39:44 +0100

    home, call and vocal fragment

--- app/src/main/java/com/example/userside/MainActivity.kt
+++ app/src/main/java/com/example/userside/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.userside
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.userside.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+        navController = navHostFragment.navController
+        //NavigationUI.setupWithNavController(binding.navBottom, navController)
     }
 }
